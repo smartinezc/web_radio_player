e2fb5655f9d724d6afdcb180d234b1c829cee92b
Santiago Martínez <ssantiago.mc@outlook.com>
2022-06-16 11:10:58 -0500

    Disc shape to Radio Images

--- lib/widgets/radioDisc.dart
+++ lib/widgets/radioDisc.dart
@@ -10,35 +10,60 @@ class RadioDisc extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: size,
-      width: size,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          width: 4,
-          // color: kSecondaryColor,
-          color: kSecondaryColor.withOpacity(0.2),
-          // color: const Color(0x33666666),
-          style: BorderStyle.solid,
-        ),
-        boxShadow: const [
-          BoxShadow(
-            color: Color(0x40000000),
-            blurRadius: 4,
-            offset: Offset(0, 4),
+    return ClipPath(
+      clipper: RadioDiscClipper(),
+      child: Container(
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            width: 4,
+            color: kSecondaryColor.withOpacity(0.8),
+            style: BorderStyle.solid,
           ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: defBorderCircle,
-        child: Image.network(
-          image,
-          fit: BoxFit.cover,
-          // loadingBuilder: (ctx, widgetChild, chunk) => Container(color: kPrimaryColor,),
-          // errorBuilder: ((context, error, stackTrace) => Image.asset("")),
+          image: DecorationImage(
+            image: NetworkImage(image),
+            fit: BoxFit.cover,
+          ),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x40000000),
+              blurRadius: 4,
+              offset: Offset(0, 4),
+            ),
+          ],
+        ),
+        child: LayoutBuilder(
+          builder: (ctx, constraints) {
+            return Center(
+              child: Container(
+                height: constraints.maxWidth*0.26,
+                decoration: const BoxDecoration(
+                  color: kSecondaryColor,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
   }
+}
+
+class RadioDiscClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    return Path()
+      ..addRect(Rect.fromLTWH(0.0, 0.0, size.width+20, size.height+20))
+      ..addOval(Rect.fromCircle(center: Offset(size.width/2, size.height/2), radius: size.width*0.11))
+      ..fillType = PathFillType.evenOdd;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) {
+    return false;
+  }
+
 }
